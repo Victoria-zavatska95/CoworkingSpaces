ab966ecda7a8d5d46810ae98755a2bee95de23d2
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-11 01:15:42 +0300

    created view new for orderspacescontroller, added action create, removed attribute integer from orderspaces model, added association orderspaces to user

--- app/controllers/orderspaces_controller.rb
+++ app/controllers/orderspaces_controller.rb
@@ -2,5 +2,18 @@ class OrderspacesController < ApplicationController
 def new
 		@orderspace = Orderspace.new
 		@coworkingspace = Coworkingspace.find(params[:coworkingspace_id])
-	end 
+	end
+
+	def create
+		@orderspace = current_user.orderspaces.build(orderspaceparams)
+		if @orderspace.save
+		redirect_to coworkingspaces_path
+		else
+		render 'new'
+		end	
+	end	
+
+	private def orderspaceparams
+		params.require(:orderspace).permit(:user_id, :coworkingspace_id, :peopleNumber, :beginDate, :finishDate, :totalSum)
+	end	
 end
---
+++ db/migrate/20181210203133_remove_integer_from_orderspaces.rb
@@ -0,0 +1,4 @@
+class RemoveIntegerFromOrderspaces < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
