83bbd53ba29e1123f07394f392cf25ed3a72a940
VictoriaZavatska <victoriazavacka8@gmail.com>
2018-12-11 15:30:52 +0200

    added custom action calculate, route for the action,  changed view orderspaces/new, added number_field and link_to calculate

--- app/controllers/orderspaces_controller.rb
+++ app/controllers/orderspaces_controller.rb
@@ -11,9 +11,19 @@ def new
 		else
 		render 'new'
 		end	
-	end	
+	end
 
 	private def orderspaceparams
 		params.require(:orderspace).permit(:user_id, :coworkingspace_id, :peopleNumber, :beginDate, :finishDate, :totalSum)
 	end	
+
+	def calculate
+		@coworkingspace = Coworkingspace.find(params[:coworkingspace_id])
+		@orderspace = Orderspace.new
+		@orderspace.totalSum = 0
+		respond_to do |format|
+		format.html { render :template => "orderspaces/new" }
+		format.json { head :no_content }
+	end	
+end
 end
