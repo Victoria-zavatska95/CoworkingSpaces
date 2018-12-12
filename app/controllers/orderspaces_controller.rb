09a75316a03cbf4626d2682742c386d1a395da1d
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-12 10:23:02 +0300

    added action and view show for orderspaces controller, global variable coworkingspace in orderspaces controller

--- app/controllers/orderspaces_controller.rb
+++ app/controllers/orderspaces_controller.rb
@@ -1,14 +1,27 @@
 class OrderspacesController < ApplicationController
-	before_action :find_order!, only: [:show]
-def new
+	before_action :find_order, only: [:show]
+$coworkingspace = nil
+			
+			
+	def new
+			$coworkingspace = Coworkingspace.find(params[:coworkingspace_id])
+				# binding.pry
 		@orderspace = Orderspace.new
-		@coworkingspace = Coworkingspace.find(params[:coworkingspace_id])
-		@orderspace.coworkingspace_id = @coworkingspace.id
+	
+		# params[:order_space][:coworkingspace_id] = @coworkingspace.id
+		# @orderspace.coworkingspace_id = @coworkingspace.id
+		# @orderspace.coworkingspace_id = Coworkingspace.find(params[:coworkingspace_id])
 	end
 
 	def create
+    params[:orderspace][:user_id] = current_user.id
+    params[:orderspace][:coworkingspace_id] = $coworkingspace.id
+    @orderspace = Orderspace.new(orderspaceparams)
+		# @orderspace = current_user.orderspaces.build(orderspaceparams)
+		# binding.pry
+		# @orderspace.coworkingspace_id = $coworkingspace.id
 
-		@orderspace = current_user.orderspaces.build(orderspaceparams)
+		# @orderspace.coworkingspace_id = Coworkingspace.find(params[:coworkingspace_id])
 		if @orderspace.save
 		redirect_to orderspace_path(@orderspace.id)
 		else
