4ed4c4638853eb2c6d8efdba92f54a69a50ce14b
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-13 10:07:12 +0300

    Added validation for available people number in orderspace model

--- app/models/orderspace.rb
+++ app/models/orderspace.rb
@@ -1,4 +1,22 @@
 class Orderspace < ApplicationRecord
 	belongs_to :user
 	belongs_to :coworkingspace
+
+	validate :available_people_number
+
+	def available_people_number
+		peopleTaken = 0
+		coworkingspace.orderspaces.each do |order|
+			if 	!(beginDate > order.finishDate || finishDate < order.beginDate)
+		peopleTaken += order.peopleNumber
+	end
+	end
+	peopleAvailable = coworkingspace.peopleNumber - peopleTaken
+	
+	if peopleTaken + peopleNumber > coworkingspace.peopleNumber
+		errors.add(:peopleNumber, 'You cannot to book this cowoking place for #{peopleNumber} people. This space is available only for #{peopleAvailable} more people')
+			throw(:abort)
+	else
+	end
+	end	
 end
