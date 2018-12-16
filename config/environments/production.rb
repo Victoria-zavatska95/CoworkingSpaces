8b4e0feafe2e362d208c8a5b91479f67d318add4
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-16 17:58:24 +0300

    added default_url_options for action_mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://coworkingspace.herokuapp.com'}
   
   config.action_mailer.smtp_settings = {
     user_name:      'viktoriacoworkingspace@gmail.com',
