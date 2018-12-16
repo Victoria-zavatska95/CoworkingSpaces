461aba4d15c8b72d1c6461370011cccd2c8bfb20
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-16 17:50:42 +0300

    added gem rails_12factor, added confirmable for users, generated vies for devise, set delivery options for environments development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,20 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    user_name:      'viktoriacoworkingspace@gmail.com',
+    password:       'spaceruby',
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
