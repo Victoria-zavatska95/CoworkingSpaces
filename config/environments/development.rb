caf250a012365790b1e7be0d72ad1a4f2d621e36
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-17 02:35:41 +0300

    wrote technical task in README, added not required confirmation in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,21 +27,21 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
   #mailer
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_options = { from: "viktoriacoworkingspace@gmail.com" }
-  # devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.smtp_settings = {
-    user_name:      'viktoriacoworkingspace@gmail.com',
-    password:       'spaceruby',
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = false
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.default_options = { from: "viktoriacoworkingspace@gmail.com" }
+  # # devise
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      'viktoriacoworkingspace@gmail.com',
+  #   password:       'spaceruby',
+  #   domain:         'gmail.com',
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
--- db/migrate/20181204211000_devise_create_users.rb
+++ db/migrate/20181204211000_devise_create_users.rb
@@ -20,7 +20,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
-
+      if Rails.env.production?
       # Confirmable
       t.string   :confirmation_token
       t.datetime :confirmed_at
@@ -31,14 +31,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
-
+    end
 
       t.timestamps null: false
+
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+
+    if Rails.env.production?
     add_index :users, :confirmation_token,   unique: true
     add_index :users, :unlock_token,         unique: true
+  end
   end
 end
