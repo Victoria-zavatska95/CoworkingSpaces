f8e90e77b7d825e1dc8ed7054bfc573ffa4ca798
VictoriaZavatska <viktoriazavacka8@gmail.com>
2018-12-21 13:46:49 +0300

    validation date is not past

--- app/models/orderspace.rb
+++ app/models/orderspace.rb
@@ -1,8 +1,16 @@
 class Orderspace < ApplicationRecord
 	belongs_to :user
 	belongs_to :coworkingspace
-
 	validate :available_people_number
+	validate :date_is_not_past
+	validate :date_consistency
+
+	def date_consistency
+			if beginDate > finishDate
+			errors.add(:beginDate, 'Begin date should be less or equal to finish date')
+			throw(:abort)	
+		end	
+	end	
 
 	def available_people_number
 		peopleTaken = 0
@@ -25,4 +33,11 @@ class Orderspace < ApplicationRecord
 		end
 	 
 	end	
+
+	def date_is_not_past
+			if beginDate < Date.today || finishDate < Date.today
+				errors.add(:beginDate, 'Such date is past')
+			throw(:abort)
+		    end
+	    end	
 end
